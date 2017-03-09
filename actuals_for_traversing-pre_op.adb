a9f3886490c6de92a8ff3c2294e662098cbed7ba
Charles A. Reynolds <reynolds12@llnl.gov>
2017-03-09 09:25:57 -0800

    Added style_checker task_1 files

--- actuals_for_traversing-pre_op.adb
+++ actuals_for_traversing-pre_op.adb
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------------------
 --                                                                          --
---                    ASIS APPLICATION TEMPLATE COMPONENTS                  --
+--                       ASIS TUTORIAL COMPONENTS                           --
 --                                                                          --
 --           A C T U A L S _ F O R _ T R A V E R S I N G . P R E _ O P      --
 --                                                                          --
@@ -20,10 +20,15 @@
 -- Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, --
 -- USA.                                                                     --
 --                                                                          --
--- ASIS Application Templates were developed and are now maintained by Ada  --
--- Core Technologies Inc (http://www.gnat.com).                             --
+-- ASIS Tutorial was developed and are now maintained by Ada Core           --
+-- Technologies Inc (http://www.gnat.com).                                  --
 --                                                                          --
 ------------------------------------------------------------------------------
+
+--  This is the body of Pre_Op to be used as an example of the style
+--  checker solution (Task 1) built on top of the ASIS Application Templates
+--  provided in ASIS-for-GNAT. This file is supposed to replace the file with
+--  the same name which is a part of the ASIS Application Templates
 
 with Ada.Wide_Text_IO;
 with Ada.Characters.Handling;
@@ -32,6 +37,10 @@ with Ada.Exceptions;
 with Asis.Exceptions;
 with Asis.Errors;
 with Asis.Implementation;
+with Asis.Elements;
+with Asis.Declarations;
+
+with Style_Checker_Utilities;
 
 separate (Actuals_For_Traversing)
 procedure Pre_Op
@@ -39,8 +48,81 @@ procedure Pre_Op
    Control : in out Asis.Traverse_Control;
    State   : in out Traversal_State)
 is
+   Argument_Kind             : Asis.Element_Kinds;
+   Argument_Declaration_Kind : Asis.Declaration_Kinds;
 begin
-   null;
+   --  Note, that the code below may be rewritten in more compact way (with
+   --  the same functionality). But we prefer to go step-by-step,
+   --  demonstrating the important ASIS queries
+
+   Argument_Kind := Asis.Elements.Element_Kind (Element);
+
+   case Argument_Kind is
+
+      when Asis.A_Declaration =>
+         --  The rule to check is about a specific kinds of declarations
+         --  only - we have to check that each subprogram body has a separate
+         --  spec. So we have first to define a more specific declaration
+         --  kind of the argument Element and then we have to check our rule
+         --  for subprogram bodies only
+
+         Argument_Declaration_Kind := Asis.Elements.Declaration_Kind (Element);
+
+         case Argument_Declaration_Kind is
+
+            when Asis.A_Procedure_Body_Declaration |
+                 Asis.A_Function_Body_Declaration  =>
+
+               if Asis.Elements.Is_Nil
+                  (Asis.Declarations.Corresponding_Declaration (Element))
+               then
+                  Style_Checker_Utilities.Report_Style_Violation
+                    (The_Element => Element,
+                     Diagnosis   => "Subprogram body with no explicit spec");
+               end if;
+
+            when others =>
+               --  For the other declaration kinds we have nothing to check,
+               --  so:
+               null;
+         end case;
+
+      when others =>
+         --  Our the only rule for Task 1 is about declarations, so if
+         --  we have something which is not A_Declaration, we have nothing
+         --  to do
+         null;
+   end case;
+
+   ----------------------
+   -- Hints for Task 2 --
+   ----------------------
+
+   --  Task 2 adds two new rules to check.
+   --
+   --  The first rule is about generic associations, so you have to add one
+   --  more alternative to the external case statement - for An_Association
+   --  Element_Kinds value. Inside this alternative you first have to define
+   --  the exact association kind, and for A_Generic_Association Element you
+   --  have to check that they are in named form. In ASIS terms this means,
+   --  that the result of Asis.Expressions.Formal_Parameter query applied
+   --  to the association Element is not nil.
+   --
+   --  The second rule is about declarations in general. Actually, it has to
+   --  be checked only for declarations which can define more then one entity
+   --  (such as object declarations and parameter declarations), and this rule
+   --  is always true for other declarations (such as package declarations,
+   --  type declarations etc.) But to simplify the code needed to check this
+   --  rule, we may check it for all declaration kinds. The check itself is
+   --  very simple - you have to get the list of the names defined by a
+   --  given declaration (see the query Asis.Declarations.Names) and check
+   --  how many they are.
+   --
+   --  These comments are kept in slightly modified form in the solution we
+   --  provide for Task 2 as comments to the corresponding fragments of the
+   --  code
+
+
 exception
 
    when Ex : Asis.Exceptions.ASIS_Inappropriate_Context          |
